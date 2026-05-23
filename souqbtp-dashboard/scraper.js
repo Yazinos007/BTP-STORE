c2fa7ef00fc0c2cb51cc0824984f5b76525bf2ee
Yassine <yazinos007@gmail.com>
2026-05-24 00:13:25 +0100

    تحديث سكربت التشغيل وتفعيل التنبيه الذكي بتقرير الصفقات

--- souqbtp-dashboard/scraper.js
+++ souqbtp-dashboard/scraper.js
@@ -1,6 +1,8 @@
 const { chromium } = require('playwright');
 const { createClient } = require('@supabase/supabase-js');
 
+console.log("السكربت بدأ العمل الآن...");
+
 const supabaseUrl = 'https://emioaqamotrycdonsswv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtaW9hcWFtb3RyeWNkb25zc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNTg1MzAsImV4cCI6MjA4MDkzNDUzMH0.ARIB-gbtSz_Bk4l3tj_34GkzRWL-0grI2XDPGfkYU5g';
 const supabase = createClient(supabaseUrl, supabaseKey);
