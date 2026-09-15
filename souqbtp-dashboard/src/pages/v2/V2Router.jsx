f7c37f7002b9f6eaacfe8d15e3bc4382916ce01d
Yassine <yazinos007@gmail.com>
2026-09-15 22:21:03 +0100

    تحديثات جديدة

--- souqbtp-dashboard/src/pages/v2/V2Router.jsx
+++ souqbtp-dashboard/src/pages/v2/V2Router.jsx
@@ -6,7 +6,7 @@ import ProjectPath from './ProjectPath';
 import ContractorHR from './ContractorHR'; 
 import ContractorMarketplace from './ContractorMarketplace'; // 🛒 واجهة التسوق (للمشتري)
 import VendorStoreManager from './VendorStoreManager';       // 📦 لوحة إضافة المنتجات (للبائع)
-import SmartMessages from './SmartMessages';
+import Contractormessage from './Contractormessage';
 
 // صفحة قيد الإنشاء ذكية
 const UnderConstruction = ({ title, icon }) => {
@@ -38,7 +38,7 @@ export default function V2Router({ session, supplier }) {
         <Route path="site-reports" element={<UnderConstruction title="كاميرا الميدان والتقارير" icon="📸" />} />
         
         {/* التواصل والمشتريات */}
-        <Route path="messages" element={<SmartMessages icon="💬" />} />
+        <Route path="messages" element={<Contractormessage icon="💬" />} />
         <Route path="hr" element={<ContractorHR />} /> 
         
         {/* 🚀 السوق المتعدد الأطراف */}
