2f28f54c08a85d801f8e5ef46d3283af1c49e143
Yassine <yazinos007@gmail.com>
2026-09-22 21:03:37 +0000

    تحديثات جديدة

--- souqbtp-dashboard/src/pages/v2/V2Router.jsx
+++ souqbtp-dashboard/src/pages/v2/V2Router.jsx
@@ -4,7 +4,7 @@ import ContractorDashboard from './ContractorDashboard';
 import CostCalculator from './CostCalculator'; 
 import ProjectPath from './ProjectPath'; 
 import ContractorHR from './ContractorHR'; 
-import ContractorMarketplace from './BTPHub'; 
+import BTPHup from './BTPHub'; 
 import VendorStoreManager from './VendorStoreManager';       
 import ChatRoom from './ChatRoom';
 import ContractorCamera from './ContractorCamera';
